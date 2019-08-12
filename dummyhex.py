6f917bc2d2705c2b7e0e853c507470e9aeee9e3c
Tadahito Yao <eighttails@nifty.com>
2019-08-12 14:35:18 +0900

    コードを整理

--- dummyhex.py
+++ dummyhex.py
@@ -1,11 +1,12 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-addr = 0xce00
-
+start_addr = 0xce00
+end_addr = 0xcfff
+addr = start_addr
 while True:
     print('{:04X}:00 00 00 00 00 00 00 00:00').format(addr)
     addr+=8
     if (addr % (8*16))==0:
         print('Sum :00 00 00 00 00 00 00 00:00')
-    if addr > 0xcfff:
+    if addr > end_addr:
         break
